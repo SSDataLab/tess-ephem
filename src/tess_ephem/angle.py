d22d1050fc36a1578e51b0f6fa9a3f77b8e39898
Geert Barentsen <geert@barentsen.be>
2020-11-23 11:41:32 -0800

    🎉 Initial commit of helper functions

---
+++ src/tess_ephem/angle.py
@@ -0,0 +1,57 @@
+"""Helper functions to deal with spherical angles."""
+from typing import Callable, Sequence
+
+import numpy as np
+from numpy import pi
+from scipy.interpolate import CubicSpline
+
+
+def enforce_positive_angle(angle: float) -> float:
+    """Ensures `angle` is in the range [0, 2*pi[.
+
+    Parameters
+    ----------
+    angle : float
+        An angle given in radians.
+
+    Returns
+    -------
+    angle : float
+        Same angle guaranteed to be in the range [0, 2*pi[
+    """
+    return (angle + 2 * pi) % (2 * pi)
+
+
+def create_angle_interpolator(
+    x: Sequence[float], angle: Sequence[float], enforce_positive: bool = False
+) -> Callable[[float], float]:
+    """Returns a function to interpolate angles as a function of an independent variable.
+
+    The function returned deals appropriately with the boundary at 360-0 degrees
+    by interpolating the sine and cosine of the angle separately.
+
+    Parameters
+    ----------
+    x : Sequence[float]
+        Independent variable.
+    angle : Sequence[float]
+        Angle in degrees (i.e., the dependent variable)/
+    enforce_positive : bool
+        If `True`, ensures angle is in the range [0, 2*pi[
+
+    Returns
+    -------
+    interpf : Callable
+        Interpolation function
+    """
+    sin = np.sin(np.radians(angle))
+    cos = np.cos(np.radians(angle))
+    cs_sin = CubicSpline(x, sin)
+    cs_cos = CubicSpline(x, cos)
+
+    def interpf(x):
+        if enforce_positive:
+            return np.degrees(enforce_positive_angle(np.arctan2(cs_sin(x), cs_cos(x))))
+        return np.degrees(np.arctan2(cs_sin(x), cs_cos(x)))
+
+    return interpf
---
+++ tests/test_angle.py
@@ -0,0 +1,36 @@
+import pytest
+
+import numpy as np
+from numpy import pi
+from numpy.testing import assert_array_equal, assert_almost_equal
+from tess_ephem.angle import enforce_positive_angle, create_angle_interpolator
+
+
+@pytest.mark.parametrize(
+    "angle,expected",
+    [(0, 0), (-pi, pi), (2 * pi, 0), (-2 * pi, 0), (3 * pi, pi), (-3 * pi, pi)],
+)
+def test_enforce_positive_angle(angle, expected):
+    """Does `enforce_positive_angle` return the expected result?"""
+    assert enforce_positive_angle(angle) == expected
+
+
+def test_enforce_positive_angle_vectorized():
+    """Is `enforce_positive_angle` vectorized?"""
+    assert all(
+        enforce_positive_angle(np.array([0, 2 * pi, 4 * pi])) == np.array([0, 0, 0])
+    )
+
+
+def test_angle_interpolator():
+    x = [-10, 10]
+    angle = [-10 * pi, 10 * pi]
+    f = create_angle_interpolator(x, angle)
+    testvalues = np.array([-10, 0, 10])
+    assert_array_equal(f(testvalues), testvalues * pi)
+
+
+@pytest.mark.parametrize("x,angle", [([1, 2, 3], [0, 0, 0]), ([1, 2, 3], [1, 2, 3])])
+def test_angle_interpolator_roundtrip(x, angle):
+    f = create_angle_interpolator(x, angle)
+    assert_almost_equal(f(x), angle)
